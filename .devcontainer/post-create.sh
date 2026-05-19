988b8a125f7817c6897f8fd7735e7e154791e540
Michal Grzelak <grzelakm@outlook.com>
2026-05-19 11:56:48 +0200

    refactor: improve config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,10 +3,19 @@ set -eou pipefail
 
 # sudo usermod --shell /usr/bin/fish $USER
 
+BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
+
+if [ -f "$BREW_BIN" ]; then
+    echo "=== [Homebrew] Exists. Updating... ==="
+    $BREW_BIN update
+else
+    echo "=== [Homebrew] Not found. Installing... ==="
+    NONINTERACTIVE=1 sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 sudo chown -R vscode:vscode /home/linuxbrew/.linuxbrew/
 sudo chown -R vscode:vscode /mnt/mise-data
 
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 
